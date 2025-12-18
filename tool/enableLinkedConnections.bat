31eb350a9a3a853ba2fa8cc62e8c12766808f6b9
DonJoeX <131236163+DonJoeX@users.noreply.github.com>
2025-12-18 14:29:05 +0100

    Update enableLinkedConnections.bat

--- tool/enableLinkedConnections.bat
+++ tool/enableLinkedConnections.bat
@@ -1,1 +1,1 @@
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLinkedConnections" /t REG_DWORD /d "00000001"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLinkedConnections" /t REG_DWORD /d "00000001" /f
