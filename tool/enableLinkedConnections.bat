b733091a81f6be76bb2ee0f356a4fe5f9ed08108
DonJoeX <131236163+DonJoeX@users.noreply.github.com>
2026-01-26 21:42:37 +0100

    Update enableLinkedConnections.bat

--- tool/enableLinkedConnections.bat
+++ tool/enableLinkedConnections.bat
@@ -1,4 +1,5 @@
 @echo off
 color 30
+title EnableLinkedConnections
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLinkedConnections" /t REG_DWORD /d "00000001" /f
 pause
