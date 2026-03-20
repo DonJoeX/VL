c2780fdb917f5ce4a978c96c0249d51e2e072ff9
DonJoeX <131236163+DonJoeX@users.noreply.github.com>
2026-03-20 09:39:41 +0100

    Update tempdel.bat

--- tool/tempdel.bat
+++ tool/tempdel.bat
@@ -1,3 +1,4 @@
+@ECHO ON
 powershell -command "Get-ChildItem 'C:\Windows\SystemTemp' -Recurse | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue"
 powershell -command "Get-ChildItem 'C:\Windows\Temp' -Recurse | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue"
 powershell -command "Get-ChildItem 'C:\Users\*\AppData\Local\CrashDumps' -Recurse | Remove-Item -Force"
@@ -21,3 +22,4 @@ del C:\TSVL\Studio24Installazione\Studio24\DB\Base\Components\SQLEXPR_x64_ENU.ex
 del C:\TSVL\Studio24Installazione\Studio24\DB\Base\Components\SQLEXPR_x64_ITA.exe /q
 del C:\TSVL\Studio24Installazione\Studio24\DB\Base\Components\SSMS-Setup-ENU.exe /q
 del C:\TSVL\Studio24Installazione\Studio24\DB\Base\Components\SSMS-Setup-ITA.exe /q
+PAUSE
