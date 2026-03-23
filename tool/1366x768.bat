795df6490c018ab2e7912a063c7f026cdd6b1cc9
DonJoeX <131236163+DonJoeX@users.noreply.github.com>
2026-03-23 12:25:50 +0100

    Update 1366x768.bat

--- tool/1366x768.bat
+++ tool/1366x768.bat
@@ -3,6 +3,5 @@ color 30
 REM powershell -Command "Set-DisplayResolution -Width 1366 -Height 768 -Force"
 powershell -Command "Start-Sleep -Seconds 1"
 dir
-cd C:\Windows\System32
-setres -w 1366 -h 768 -f
+%SystemRoot%\System32\setres.exe -w 1366 -h 768 -f
 pause
