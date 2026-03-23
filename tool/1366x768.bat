89961755049dcadf21d9790377d61fc5f69a7e0a
DonJoeX <131236163+DonJoeX@users.noreply.github.com>
2026-03-23 12:33:45 +0100

    Update 1366x768.bat

--- tool/1366x768.bat
+++ tool/1366x768.bat
@@ -1,7 +1,6 @@
-@ECHO ON
+@echo on
 color 30
-REM powershell -Command "Set-DisplayResolution -Width 1366 -Height 768 -Force"
+:: Sysnative forza le app a 32-bit a vedere la vera cartella System32 a 64-bit
+C:\Windows\Sysnative\setres.exe -w 1366 -h 768 -f
 powershell -Command "Start-Sleep -Seconds 1"
-dir
-%SystemRoot%\System32\setres.exe -w 1366 -h 768 -f
 pause
