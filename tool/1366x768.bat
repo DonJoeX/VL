877c31916b5377fbca494e070a436a5fd2bdafe3
DonJoeX <131236163+DonJoeX@users.noreply.github.com>
2026-03-23 12:36:23 +0100

    Update 1366x768.bat

--- tool/1366x768.bat
+++ tool/1366x768.bat
@@ -1,6 +1,4 @@
-@echo on
+@echo off
 color 30
 :: Sysnative forza le app a 32-bit a vedere la vera cartella System32 a 64-bit
 C:\Windows\Sysnative\setres.exe -w 1366 -h 768 -f
-powershell -Command "Start-Sleep -Seconds 1"
-pause
